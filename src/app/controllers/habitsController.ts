2a8a2846da8539073fd0070a4db5c836879a817d
RondneyLoiola <rondney.loiola11@gmail.com>
2025-12-03 19:29:58 -0300

    validação com zod

--- src/app/controllers/habitsController.ts
+++ src/app/controllers/habitsController.ts
@@ -1,21 +1,49 @@
 import type { Request, Response } from "express";
 import { habitModel } from "../models/habitsModel";
+import { z } from 'zod'
 
 class HabitsController {
     store = async (req: Request, res: Response): Promise<Response> => {
+        const schema = z.object({
+            name: z.string().min(1, 'Habit name is required'),
+            
+        })
+
         const { name } = req.body
 
-        const findHabit = await habitModel.findOne({ name })
+        if (!name) {
+            return res.status(400).json({
+                message: 'Habit name is required.'
+            })
+        }
 
-        if(findHabit) {
+        const habit = schema.safeParse({
+            name,
+            
+        })
+
+        if (!habit.success) {
+            const errors = habit.error.issues.map(item => {
+
+            })
+            return res.status(400).json({
+                message: 'Habit must be a string.'
+            })
+        }
+
+        const findHabit = await habitModel.findOne({
+            name: habit.data.name
+        })
+
+        if (findHabit) {
             return res.status(400).json({
                 mensage: 'Habit already exists!'
             })
         }
 
         const newHabit = await habitModel.create({
-            name,
-            completedDates: []
+            name: habit.data.name,
+            completedDates: [],
         })
 
         return res.status(201).json(newHabit)
